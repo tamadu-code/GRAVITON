f10d753568376d31494040911715b24245137d01
tamadu-code <orukari878@gmail.com>
2026-05-12 15:45:35 +0100

    build: finalize syntax fixes and v56 cache bump

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'graviton-cache-v54';
+const CACHE_NAME = 'graviton-cache-v56';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
   './assets/css/style.css',
-  './assets/js/main.js?v=53',
+  './assets/js/main.js?v=55',
   './assets/js/ui.js',
   './assets/js/db.js',
   './assets/js/utils.js',
