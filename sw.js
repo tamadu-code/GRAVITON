21627b0b3df792c98e5ece54e5cc321222bdcb9c
tamadu-code <orukari878@gmail.com>
2026-05-20 13:19:04 +0100

    bump entire system including service worker to v260

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-const CACHE_NAME = 'graviton-cache-v259';
+const CACHE_NAME = 'graviton-cache-v260';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  './assets/css/style.css?v=259',
-  './assets/js/main.js?v=259',
-  './assets/js/ui.js?v=259',
-  './assets/js/db.js?v=259',
-  './assets/js/utils.js?v=259',
-  './assets/js/supabase-client.js?v=259',
+  './assets/css/style.css?v=260',
+  './assets/js/main.js?v=260',
+  './assets/js/ui.js?v=260',
+  './assets/js/db.js?v=260',
+  './assets/js/utils.js?v=260',
+  './assets/js/supabase-client.js?v=260',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png',
   'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Outfit:wght@400;500;600;700;800&display=swap',
