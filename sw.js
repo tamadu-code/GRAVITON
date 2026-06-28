bdeadf0f91a200644dcc9d04de3b10c1ad87544e
tamadu-code <orukari878@gmail.com>
2026-06-28 07:58:26 +0100

    chore: bump service worker cache version to v350

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-const CACHE_NAME = 'graviton-cache-v349';
+const CACHE_NAME = 'graviton-cache-v350';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  './assets/css/style.css?v=349',
-  './assets/js/main.js?v=349',
-  './assets/js/ui.js?v=349',
-  './assets/js/db.js?v=349',
-  './assets/js/utils.js?v=349',
-  './assets/js/supabase-client.js?v=349',
+  './assets/css/style.css?v=350',
+  './assets/js/main.js?v=350',
+  './assets/js/ui.js?v=350',
+  './assets/js/db.js?v=350',
+  './assets/js/utils.js?v=350',
+  './assets/js/supabase-client.js?v=350',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png',
   'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Outfit:wght@400;500;600;700;800&display=swap',
